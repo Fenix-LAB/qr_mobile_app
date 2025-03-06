85d86967ee78fa3a01c27f8a33fd0aa5a900cc46
Fenix-LAB <fenixcr.123@gmail.com>
2025-03-05 20:51:22 -0600

    feat: add history service to fetch user event histories via GraphQL

---
+++ src/services/historyService.ts
@@ -0,0 +1,23 @@
+import { apiRequest, graphqlRequest } from "./api";
+
+// Obtener todos los historiales de un usuario (GraphQL)
+export const obtenerHistorialesUsuario = async (userId: number) => {
+  const query = `
+    query MyQuery {
+        qr_shield_event(where: {user_id: {_eq: 1}}) {
+            frac {
+            name
+            location
+            }
+            datetime
+        }
+        }
+    `;
+    
+  const response = await graphqlRequest(query);
+
+  // console.log("Respuesta GraphQL:", response); // 👈 Verifica qué devuelve la API
+
+  return response?.data?.qr_shield_event?.map((item: any) => item.frac) || [];
+};
+        
